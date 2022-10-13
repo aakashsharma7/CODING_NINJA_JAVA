065c76cc4bf6ddecb9840cc997b9e43770563da7
AakashSharma <aakashsharma.ghd@gmail.com>
2022-10-13 19:57:30 +0530

    .\Leaders_in_Array\

---
+++ Test_2/Leaders_in_Array.java
@@ -0,0 +1,21 @@
+package Test_2;
+
+public class Leaders_in_Array {
+    public static void leaders(int[] input) {
+        for(int i=0;i<input.length;i++) {
+         boolean allSmaller=true;
+         for(int j=i+1;j<input.length;j++) {
+             if(input[i] < input[j]) {
+                 allSmaller=false;
+                 break;
+             }
+         }
+         if(allSmaller) {
+             System.out.print(input[i]+" ");
+         }
+     }
+     
+ }
+ 
+}
+
