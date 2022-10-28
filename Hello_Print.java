37c1e7baad5a3f09e9f88d4a1f51e6999b71c656
AakashSharma <aakashsharma.ghd@gmail.com>
2022-10-28 23:14:39 +0530

    .\Hello_Print.java

---
+++ Hello_Print.java
@@ -0,0 +1,8 @@
+public class Hello_Print {
+        public static void main(String[] args){
+           // if you use println in java will put a new line at a end.
+            System.out.println("Helloworld");
+            }
+    }
+    
+
